2742f894297c83cca0aebfaf51ceb1a58bc80cc9
ishanyaa <ishanyaiiserb@gmail.com>
2025-09-05 15:56:28 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,34 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  // 👇 IMPORTANT for GitHub Pages
+  base: "/ishanya-portfolio/",
+}));
+
+
+
+
+/*
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
@@ -20,3 +48,4 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+*/
