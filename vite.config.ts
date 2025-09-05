71c605a99fb2938f79e14dd425900336b3939440
ishanyaa <ishanyaiiserb@gmail.com>
2025-09-05 17:48:53 +0530

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -19,32 +19,5 @@ export default defineConfig(({ mode }) => ({
     },
   },
   // IMPORTANT for GitHub Pages (root domain repo)
-  base: "./",
+  base: "/",
 }));
-
-
-
-/*
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
-*/
